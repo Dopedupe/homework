e1cd2a5429977916ddbbfd0661eac51d28beaccd
Dopedupe <94796254+Dopedupe@users.noreply.github.com>
2022-10-18 18:46:59 +0300

    fixed 2.1

--- task2.1.py
+++ task2.1.py
@@ -5,34 +5,26 @@ def check(text):
                 pass
             else:
                 return False
-        
+
     return True
 
 def main():
     a = input("Введите строку\n")
     b = ""
     c = ""
-    count = 1
-    max_count = 0
-    for i in range(1,len(a)):
-        if (a[i-1] != a[i]):
-            b = b + a[i-1] + a[i]
-            if (check(b)):
-                b = b[:-1:]
-                count += 1
-            else:
-                b = b[1:-1:]
+    for i in range(0,len(a)):
+        b = a[i::]
+        if (check(b)):
+            if len(c) < len(b):
                 c = b
                 b = ""
-                max_count = count
-                count = 1
-        
-    
-    if max_count == 0 or max_count < count:
-        print(b + a[i])
-    else:
-        print(c) 
-    
+    for i in range(0,len(a)):
+        b = a[:i:]
+        if (check(b)):
+            if len(c) < len(b):
+                c = b
+                b = ""
+    print(c)
+
 if __name__ == "__main__":
     main()
-    
