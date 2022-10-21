0bccd41b6d0d3cbdce55b381381654f64e748f5f
Dopedupe <94796254+Dopedupe@users.noreply.github.com>
2022-10-22 02:21:19 +0300

    fixed 1.2

--- task1.2.py
+++ task1.2.py
@@ -1,11 +1,22 @@
 def main():
     a = (input("Введите целое число\n"))
-
-    b = a[::-1]
-    b  = int(b)
-    if ((b>-2**7)and (b<2**7-1)):
-        print ("solution eists")
+    if a[0] == '-':
+        c = a[:0] + a[1:]
+        b = c[::-1]
+        b = a[0] + b
+        b  = int(b)
+        print(b)
+        if ((b>-2**7)and (b<2**7-1)):
+            print ("solution eists")
+        else:
+            print("no solution")
     else:
-        print("no solution")
+        b = a[::-1]
+        b  = int(b)
+        print(b)
+        if ((b>-2**7)and (b<2**7-1)):
+            print ("solution exists")
+        else:
+            print("no solution")
 if __name__=='__main__':
     main()
