ed25642840b577760c77302b315f6ac03fc596c9
Dopedupe <94796254+Dopedupe@users.noreply.github.com>
2022-10-22 21:41:43 +0300

    folders

---
+++ пр1/task 2.3.py
@@ -0,0 +1,24 @@
+
+
+
+
+def longest_valid(s):
+    match = [0] * (len(s) + 1)
+    for i in range(1, len(s)):
+        if s[i] in '({[':
+            continue
+        open = '({['[')}]'.index(s[i])]
+        start = i - 1 - match[i - 1]
+        if start < 0:
+            continue
+        if s[start] != open:
+            continue
+        match[i] = i - start + 1 + match[start - 1]
+    best = max(match)
+    end = match.index(best)
+    return s[end + 1 - best:end + 1]
+
+print(longest_valid("()(({}[](][{[()]}]{})))("))
+
+# if __name__ =="__main__":
+#     main()
