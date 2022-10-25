7bbd78992ef05508ba0c84bd9ae3f036e849c5a6
Dopedupe <94796254+Dopedupe@users.noreply.github.com>
2022-10-25 22:24:06 +0300

    added tasks

--- пр5/task 5.2.py
+++ пр5/task 5.2.py
@@ -1,16 +1,19 @@
 from itertools import combinations
-A = [1,2,3,4,4]
-res = set ()
-A = set (A)
-for x in range(1,len (A) +1):
-    temp = combinations (A, x)
-    for i in (set (temp)) :
-        res.add (i)
-res = sorted (sorted (res) , key=len)
-ress ="{"
-for i in res:
-    i = set(i)
-    ress += str(i).replace(' ','') +','
-ress = ress [:-1:] +"}"
-print(ress)
-print(len(res))
+def main():
+    a = [1,2,3,4,4]
+    res = set ()
+    a = set (a)
+    for x in range(1,len (a) +1):
+        temp = combinations (a, x)
+        for i in (set (temp)) :
+            res.add (i)
+    res = sorted (sorted (res) , key=len)
+    ress ="{"
+    for i in res:
+        i = set(i)
+        ress += str(i).replace(' ','') +','
+    ress = ress [:-1:] +"}"
+    print(ress)
+    print(len(res))
+if __name__ =='__main__':
+    main()
